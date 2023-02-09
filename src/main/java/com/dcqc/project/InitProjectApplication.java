a0a5715a801732ce734159fbf31f780d45e37361
pingspong <pingspong@outlook.com>
2023-02-09 20:49:39 +0800

    Init the SpringBoot Project

---
+++ src/main/java/com/dcqc/project/InitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dcqc.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dcqc/project/InitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcqc.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
